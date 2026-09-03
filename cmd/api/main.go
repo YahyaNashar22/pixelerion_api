4237d6bfa21b720bfa6726444b4050277f108f7b
YahyaNashar22 <yahyanashar22@gmail.com>
2026-09-03 12:15:37 +0300

    feat: added server timeout configuration

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -4,6 +4,7 @@ import (
 	"log/slog"
 	"net/http"
 	"os"
+	"time"
 )
 
 func main() {
@@ -21,6 +22,22 @@ func main() {
 	server := &http.Server{
 		Addr:    ":8080",
 		Handler: mux,
+
+		//Limits the amount of time the server will wait to read just the HTTP request headers
+		// This the primary defense against Slowloris attacks
+		ReadHeaderTimeout: 5 * time.Second,
+
+		//  Limits the total time allowed to read the entire request
+		// It prevents clients from hanging up the server while uploading massive or trickling data streams
+		ReadTimeout: 15 * time.Second,
+
+		// Limits the amount of time the server has to write the response back to the client
+		// If a client has a terribly slow download speed, or if your handler gets stuck processing a heavy database query before writing a response, this ensures the connection doesn't stay open forever
+		WriteTimeout: 30 * time.Second,
+
+		// Controls how long a Keep-Alive connection can stay open while waiting for the next request.
+		// Reusing connections saves CPU and latency, but leaving them open indefinitely would quickly exhaust the server's maximum open file descriptors.
+		IdleTimeout: 60 * time.Second,
 	}
 
 	logger.Info(
