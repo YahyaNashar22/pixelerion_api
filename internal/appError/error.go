503eb0a942972b8ecf4c6df5cf0a57ae20bafdec
YahyaNashar22 <yahyanashar22@gmail.com>
2026-09-23 14:05:18 +0300

    feat: added centralized error handling setup

---
+++ internal/appError/error.go
@@ -0,0 +1,32 @@
+package appError
+
+import "fmt"
+
+type Code string
+
+const (
+	CodeValidation   Code = "VALIDATION_ERROR"
+	CodeUnauthorized Code = "UNAUTHORIZED"
+	CodeForbidden    Code = "FORBIDDEN"
+	CodeNotFound     Code = "NOT_FOUND"
+	CodeConflict     Code = "CONFLICT"
+	CodeInternal     Code = "INTERNAL_ERROR"
+)
+
+type Error struct {
+	Code    Code
+	Message string
+	Err     error
+}
+
+func (e *Error) Error() string {
+	if e.Err != nil {
+		return fmt.Sprintf("%s: %v", e.Message, e.Err)
+	}
+
+	return e.Message
+}
+
+func (e *Error) Unwrap() error {
+	return e.Err
+}
